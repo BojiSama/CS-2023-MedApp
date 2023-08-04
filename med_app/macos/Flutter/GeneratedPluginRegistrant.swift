3bbaf7493dddc9e6b8b59a230c7912f7f1b80cb7
Bwogi Francis <francisbwogi.fb@gmail.com>
2023-08-04 08:37:24 +0300

    Voice Search Trial

--- med_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ med_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_speech
 import geolocator_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSpeechRecognitionPlugin.register(with: registry.registrar(forPlugin: "FlutterSpeechRecognitionPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
