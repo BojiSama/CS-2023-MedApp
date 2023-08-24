4a59841330b1b00417c888430b40713193210f80
magnus501 <barrykawooya@gmail.com>
2023-08-24 08:17:29 +0300

    final

--- med_app/lib/pages/call_help.dart
+++ med_app/lib/pages/call_help.dart
@@ -1,10 +1,15 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:fast_contacts/fast_contacts.dart';
+import 'package:med_app/screens/panic_page.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+class NextOfKin {
+  final Contact contact;
+
+  const NextOfKin(this.contact);
+}
 
 class CallHelp extends StatefulWidget {
   const CallHelp({super.key});
@@ -18,73 +23,128 @@ class CallHelp extends StatefulWidget {
 class _CallHelpState extends State<CallHelp> {
   String? _status;
 
+  List<Contact> contactsFiltered = [];
+
+  final searchController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    searchController.addListener(() { 
+      filterContacts();
+    });
+  }
+
+  filterContacts() async {
+    List<Contact> _contacts = [];
+    _contacts.addAll(await getContacts());
+    if(searchController.text.isNotEmpty){
+      _contacts.retainWhere((contact){
+        String searchTerm = searchController.text.toLowerCase();
+        String contactName = searchController.text.toLowerCase();
+        return contactName.contains(searchTerm);
+      });
+    }
+
+    setState(() {
+      contactsFiltered = _contacts;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isSearching = searchController.text.isNotEmpty;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Call for Help'),
       ),
-      body: SafeArea(
-        // height: double.infinity,
-        child: FutureBuilder(
-          future: getContacts(),
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                Contact contact = snapshot.data[index];
-                return Column(
-                  children: [
-                    ListTile(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      // tileColor: Colors.red[100],
-                      leading: const CircleAvatar(
-                        radius: 20.0,
-                        child: Icon(Icons.person),
-                      ),
-                      title: Text(contact.displayName),
-                      subtitle: Text(contact.phones[0].number),
-                      trailing: TextButton(
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 32,
-                            vertical: 12,
+      body: Column(
+        children: [
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: TextField(
+          //     controller: searchController,
+          //     decoration: InputDecoration(
+          //       labelText: 'Search',
+          //       border: OutlineInputBorder(
+          //         borderSide: BorderSide(
+          //           color: Theme.of(context).primaryColor,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          Expanded(
+            child: FutureBuilder(
+              future: getContacts(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: isSearching == true ? contactsFiltered.length : snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    Contact contact = isSearching == true ? contactsFiltered[index] : snapshot.data[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
-                          shape: const RoundedRectangleBorder(
-                            side: BorderSide(
-                              color: Colors.redAccent,
+                          // tileColor: Colors.red[100],
+                          leading: const CircleAvatar(
+                            radius: 20.0,
+                            child: Icon(Icons.person),
+                          ),
+                          title: Text(contact.displayName),
+                          subtitle: Text(contact.phones[0].number),
+                          onTap: () {
+                            Navigator.pop(context);
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (_) => ReceiveContact(contact: contact),
+                            //   ),
+                            // );
+                          },
+                          trailing: TextButton(
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 32,
+                                vertical: 12,
+                              ),
+                              shape: const RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.redAccent,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12.0)),
+                              ),
                             ),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(12.0)),
+                            child: const Text('call'),
+                            onPressed: () async {
+                              await FlutterPhoneDirectCaller.callNumber(
+                                contact.phones[0].number,
+                              );
+                            },
                           ),
                         ),
-                        child: const Text('call'),
-                        onPressed: () async {
-                          await FlutterPhoneDirectCaller.callNumber(
-                            contact.phones[0].number,
-                          );
-                        },
-                      ),
-                    ),
-                    const Divider(
-                      height: 1.0,
-                      color: Colors.red,
-                      indent: 20.0,
-                      endIndent: 20.0,
-                    ),
-                  ],
+                        const Divider(
+                          height: 1.0,
+                          color: Colors.red,
+                          indent: 20.0,
+                          endIndent: 20.0,
+                        ),
+                      ],
+                    );
+                  },
                 );
               },
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- med_app/lib/screens/panic_page.dart
+++ med_app/lib/screens/panic_page.dart
@@ -1,5 +1,9 @@
+import 'package:fast_contacts/fast_contacts.dart';
 import 'package:flutter/material.dart';
 import '../pages/call_help.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+
+// import 'package:settings_button'
 
 class PanicPage extends StatefulWidget {
   const PanicPage({super.key});
@@ -12,7 +16,46 @@ class PanicPage extends StatefulWidget {
 class _PanicPageState extends State<PanicPage> {
   @override
   Widget build(BuildContext context) {
+    // final nextofkin = ModalRoute.of(context)!.settings.arguments as Contact;
+
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                height: 20.0,
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.phone_callback,
+                      color: Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 5.0,
+                    ),
+                    // Text('set panic contact'),
+                    SizedBox(
+                      height: 30.0,
+                      child: TextButton(
+                        onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => const CallHelp(),
+                          ),
+                        ),
+                        child: const Text('set panic contact'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+        title: const Text('call for help'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -27,12 +70,19 @@ class _PanicPageState extends State<PanicPage> {
                   style: ElevatedButton.styleFrom(
                     shape: const CircleBorder(),
                   ),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const CallHelp(),
-                    ),
-                  ),
+                  onPressed: () async {
+                    await FlutterPhoneDirectCaller.callNumber(
+                      // ReceiveContact(contact: Contact.fromMap({})).contact.phones[0].number,
+                      // contact!.phones[0].number,
+                      '0789882274'
+                    );
+                  },
+                  // onPressed: () => Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (_) => const CallHelp(),
+                  //   ),
+                  // ),
                   child: const Text(
                     'HELP',
                     style: TextStyle(
@@ -45,6 +95,78 @@ class _PanicPageState extends State<PanicPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ReceiveContact extends StatelessWidget {
+  const ReceiveContact({super.key, required this.contact});
+
+  final Contact contact;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const PanicPage();
+  }
+}
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextButton(
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => Dialog(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('This is a typical dialog.'),
+                    const SizedBox(height: 15),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          child: const Text('Show Dialog'),
+        ),
+        const SizedBox(height: 10),
+        TextButton(
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => Dialog.fullscreen(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('This is a fullscreen dialog.'),
+                  const SizedBox(height: 15),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          child: const Text('Show Fullscreen Dialog'),
+        ),
+      ],
     );
   }
 }
